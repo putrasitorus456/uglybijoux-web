19ae59ef7ebd0ba904a9ea592cb68dfdb15f543d
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2025-04-11 13:26:31 +0700

    eslint error

--- app/products/[slug]/page.tsx
+++ app/products/[slug]/page.tsx
@@ -2,8 +2,9 @@ import products from "@/lib/product-all";
 import { notFound } from "next/navigation";
 import ClientProductDetail from "./ClientProductDetail";
 
-export default async function ProductDetailPage({ params }: { params: unknown }) {
-  const slug = (await params as { slug: string }).slug;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default async function ProductDetailPage({ params }: { params: any }) {
+  const slug = (await params).slug;
 
   const product = products.find((p) => p.slug === slug);
 
