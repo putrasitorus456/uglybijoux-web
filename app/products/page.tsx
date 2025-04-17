693ae71d8e241a14c51a860044c9c63c82d10f42
putrasitorus456 <putra.agung0304@mail.ugm.ac.id>
2025-04-17 13:12:26 +0700

    deleting some text

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -3,21 +3,12 @@
 
 import React, { Suspense } from "react";
 import ProductPageContent from "./ProductPageContent"; // ⬅️ new file/component
-import FadeIn from "@/components/FadeIn";
 
 // Top-level component
 export default function ProductPage() {
   return (
-    <main className="bg-white text-black font-anodina">
+    <main className="pt-32 bg-white text-black font-anodina">
       {/* Hero Title Section */}
-      <section className="text-center px-6 sm:px-10 md:px-20 pt-32 pb-12">
-        <FadeIn yOffset={30}>
-          <h1 className="text-[20px] sm:text-[20px] md:text-[25px] font-semibold tracking-tight max-w-4xl mx-auto">
-            Explore UglyBijoux.
-          </h1>
-        </FadeIn>
-      </section>
-
       {/* Suspense-wrapped dynamic content */}
       <Suspense fallback={<p className="text-center">Loading products...</p>}>
         <ProductPageContent />
